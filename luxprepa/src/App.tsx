a5e9e0097c9e5fba8c3211ed95990f16f8d7ef80
jojo1227988 <demeualima74@gmail.com>
2026-05-20 17:48:47 +0200

    first config

--- luxprepa/src/App.tsx
+++ luxprepa/src/App.tsx
@@ -1,9 +1,12 @@
-
+import { BiSun } from "react-icons/bi"
 function App() {
 
   return (
     <>
-    Bonjour!
+    <div className="p-4 items-center bg-blue-500 rounded-lg flex flex-row">
+      Bonjour viviane!
+      <BiSun color="yellow" size={30}/>
+    </div>
     </>
   )
 }
