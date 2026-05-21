6a63613aeceb79376a09e66a6fc888bba88df4da
jojo1227988 <demeualima74@gmail.com>
2026-05-21 13:47:58 +0200

    Ajout de la page concours et verification de code

--- luxprepa/src/App.tsx
+++ luxprepa/src/App.tsx
@@ -2,7 +2,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Login";
 import Register from "./Register";
 import Home from "./Home";
-
+import Concours from "./Concours";
+import VerifyCode from "./Code";
 function App() {
   return (
     <BrowserRouter>
@@ -10,6 +11,8 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register"element={<Register />} />
+        <Route path="/concours"element={<Concours />} />
+        <Route path="/verify"element={<VerifyCode />} />
       </Routes>
     </BrowserRouter>
   );
